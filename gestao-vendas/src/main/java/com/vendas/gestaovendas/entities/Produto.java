a1c9db5e7df61bf97f0a7bc2ada14ab4abea8a6e
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-02-19 14:22:03 -0300

    session 2 classroom mapped enteties with JPA

---
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/entities/Produto.java
@@ -0,0 +1,119 @@
+package com.vendas.gestaovendas.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "preco_custo")
+    private BigDecimal precoCusto;
+
+    @Column(name = "preco_venda")
+    private BigDecimal precoVenda;
+
+    @Column(name = "observacao")
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_categoria", referencedColumnName = "codigo")
+    private Categoria categoria;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(BigDecimal precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public BigDecimal getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(BigDecimal precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, descricao, quantidade, precoCusto, precoVenda, observacao, categoria);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(codigo, produto.codigo) && Objects.equals(descricao, produto.descricao) &&
+                Objects.equals(quantidade, produto.quantidade) && Objects.equals(precoCusto, produto.precoCusto) &&
+                Objects.equals(precoVenda, produto.precoVenda) && Objects.equals(observacao, produto.observacao) &&
+                Objects.equals(categoria, produto.categoria);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", descricao='" + descricao + '\'' +
+                ", quantidade=" + quantidade +
+                ", precoCusto=" + precoCusto +
+                ", precoVenda=" + precoVenda +
+                ", observacao='" + observacao + '\'' +
+                ", categoria=" + categoria +
+                '}';
+    }
+}
