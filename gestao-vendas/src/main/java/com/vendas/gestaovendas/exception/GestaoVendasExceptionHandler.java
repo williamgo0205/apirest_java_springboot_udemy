6bf1a085add7a992449583c39a55a66d4c986d79
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-02-27 12:39:17 -0300

    session 3 classroom 12 - Adjusts comments class Error

--- gestao-vendas/src/main/java/com/vendas/gestaovendas/exception/GestaoVendasExceptionHandler.java
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/exception/GestaoVendasExceptionHandler.java
@@ -1,15 +1,29 @@
 package com.vendas.gestaovendas.exception;
 
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
@@ -22,6 +36,25 @@ public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler
     public static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
     public static final String CONSTANT_VALIDATION_LENGTH = "Length";
 
+    /**
+     *  Classe de extensao ResponseEntityExceptionHandler consegue tratar as seguintes classes de erro
+     *     HttpRequestMethodNotSupportedException .class,
+     *     HttpMediaTypeNotSupportedException .class,
+     *     HttpMediaTypeNotAcceptableException .class,
+     *     MissingPathVariableException .class,
+     *     MissingServletRequestParameterException .class,
+     *     ServletRequestBindingException .class,
+     *     ConversionNotSupportedException .class,
+     *     TypeMismatchException .class,
+     *     HttpMessageNotReadableException .class,
+     *     HttpMessageNotWritableException .class,
+     *     MethodArgumentNotValidException.class,
+     *     MissingServletRequestPartException .class,
+     *     BindException .class,
+     *     NoHandlerFoundException .class,
+     *     AsyncRequestTimeoutException .class
+     *
+     */
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
@@ -31,9 +64,11 @@ public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler
         return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
     }
 
-    // Excecao EmptyResultDataAccessException não existente na extensao ResponseEntityExceptionHandler
-    // por esse motivo foi criada com a anotacao  @ExceptionHandler(EmptyResultDataAccessException.class)
-    // indicando a classe de excecao
+    /**
+     * Excecao EmptyResultDataAccessException não existente na extensao ResponseEntityExceptionHandler
+     * por esse motivo foi criada com a anotacao  @ExceptionHandler(EmptyResultDataAccessException.class)
+     * indicando a classe de excecao
+     */
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
                                                                         WebRequest request){
