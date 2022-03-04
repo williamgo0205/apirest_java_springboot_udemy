5471308e89399b4769014370963caddcdae0c51c
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-03-04 08:34:43 -0300

    Refactoring basePackage EntityScan with Application project

--- gestao-vendas/src/main/java/com/vendas/gestaovendas/GestaoVendasApplication.java
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/GestaoVendasApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"com.vendas.gestaovendas.entities"})
+@EntityScan(basePackages = {"com.vendas.gestaovendas.entity"})
 @EnableJpaRepositories(basePackages = {"com.vendas.gestaovendas.repository"})
 @ComponentScan(basePackages = {"com.vendas.gestaovendas.service",
         "com.vendas.gestaovendas.controller",
