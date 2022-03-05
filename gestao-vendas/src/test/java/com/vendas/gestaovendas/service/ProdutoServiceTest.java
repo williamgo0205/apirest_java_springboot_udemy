8245de0758f25489e89dbe9a5e0d1d3002a70b3c
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-03-05 10:30:06 -0300

    Adding unit tests insert and update ProdutoService

--- gestao-vendas/src/test/java/com/vendas/gestaovendas/service/ProdutoServiceTest.java
+++ gestao-vendas/src/test/java/com/vendas/gestaovendas/service/ProdutoServiceTest.java
@@ -2,6 +2,7 @@ package com.vendas.gestaovendas.service;
 
 import com.vendas.gestaovendas.entity.Categoria;
 import com.vendas.gestaovendas.entity.Produto;
+import com.vendas.gestaovendas.exception.RegraNegocioException;
 import com.vendas.gestaovendas.repository.CategoriaRepository;
 import com.vendas.gestaovendas.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -16,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -39,7 +43,6 @@ public class ProdutoServiceTest {
     private static final BigDecimal PRECO_VENDA_PRODUTO_2 = new BigDecimal("1100");
     private static final String     OBSERVACAO_PRODUTO_2  = "Monitor Dell 24 polegadas";
 
-
     @InjectMocks
     private ProdutoService produtoService;
 
@@ -47,7 +50,7 @@ public class ProdutoServiceTest {
     private ProdutoRepository produtoRepositoryMock;
 
     @Mock
-    private CategoriaRepository categoriaRepositoryMock;
+    private CategoriaService categoriaServiceMock;
 
     @Test
     public void listarTodosProdutosTest() {
@@ -66,7 +69,7 @@ public class ProdutoServiceTest {
 
         List<Produto> ProdutoListService = produtoService.listarTodos(ID_CATEGORIA_1);
 
-        verify(produtoRepositoryMock, times(1)).findByCategoriaCodigo(any());
+        verify(produtoRepositoryMock, times(1)).findByCategoriaCodigo(anyLong());
 
         assertEquals(ProdutoListService.get(0).getCodigo(),     produto1.getCodigo());
         assertEquals(ProdutoListService.get(0).getDescricao(),  produto1.getDescricao());
@@ -85,6 +88,153 @@ public class ProdutoServiceTest {
         assertEquals(ProdutoListService.get(1).getObservacao(), produto2.getObservacao());
         assertEquals(ProdutoListService.get(1).getCategoria().getCodigo(), produto2.getCategoria().getCodigo());
         assertEquals(ProdutoListService.get(1).getCategoria().getNome(),   produto2.getCategoria().getNome());
+    }
+
+    @Test
+    public void buscaPorCodigoProdutoTest() {
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produto = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1, PRECO_CUSTO_PRODUTO_1,
+                PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        doReturn(Optional.of(produto)).when(produtoRepositoryMock).buscaPorCodigoProduto(ID_PRODUTO_1, ID_CATEGORIA_1);
+
+        Optional<Produto> optProduto = produtoService.buscaPorCodigoProduto(ID_PRODUTO_1, ID_CATEGORIA_1);
+
+        verify(produtoRepositoryMock, times(1)).buscaPorCodigoProduto(anyLong(), anyLong());
+
+        assertEquals(optProduto.get().getCodigo(),     produto.getCodigo());
+        assertEquals(optProduto.get().getDescricao(),  produto.getDescricao());
+        assertEquals(optProduto.get().getQuantidade(), produto.getQuantidade());
+        assertEquals(optProduto.get().getPrecoCusto(), produto.getPrecoCusto());
+        assertEquals(optProduto.get().getPrecoVenda(), produto.getPrecoVenda());
+        assertEquals(optProduto.get().getObservacao(), produto.getObservacao());
+        assertEquals(optProduto.get().getCategoria().getCodigo(), produto.getCategoria().getCodigo());
+        assertEquals(optProduto.get().getCategoria().getNome(),   produto.getCategoria().getNome());
+    }
+
+    @Test
+    public void salvarProdutoTest() {
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produto = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1, PRECO_CUSTO_PRODUTO_1,
+                PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        doReturn(Optional.of(categoria)).when(categoriaServiceMock).buscarPorCodigo(ID_CATEGORIA_1);
+        doReturn(Optional.of(produto)).when(produtoRepositoryMock)
+                .findByCategoriaCodigoAndDescricao(ID_CATEGORIA_1, DESCRICAO_PRODUTO_1);
+        doReturn(produto).when(produtoRepositoryMock).save(any());
+
+        Produto produtoSalvo = produtoService.salvar(ID_CATEGORIA_1, produto);
+
+        verify(categoriaServiceMock, times(1)).buscarPorCodigo(any());
+        verify(produtoRepositoryMock, times(1))
+                .findByCategoriaCodigoAndDescricao(anyLong(), anyString());
+        verify(produtoRepositoryMock, times(1)).save(any());
+
+        assertEquals(produto.getCodigo(),     produtoSalvo.getCodigo());
+        assertEquals(produto.getDescricao(),  produtoSalvo.getDescricao());
+        assertEquals(produto.getQuantidade(), produtoSalvo.getQuantidade());
+        assertEquals(produto.getPrecoCusto(), produtoSalvo.getPrecoCusto());
+        assertEquals(produto.getPrecoVenda(), produtoSalvo.getPrecoVenda());
+        assertEquals(produto.getObservacao(), produtoSalvo.getObservacao());
+        assertEquals(produto.getCategoria().getCodigo(), produtoSalvo.getCategoria().getCodigo());
+        assertEquals(produto.getCategoria().getNome(),   produtoSalvo.getCategoria().getNome());
+    }
+
+    @Test
+    public void erroSalvarProdutoCategoriaNulaTest() {
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produto = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1, PRECO_CUSTO_PRODUTO_1,
+                PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        assertThrows(RegraNegocioException.class, () -> produtoService.salvar(null, produto));
+
+        verify(produtoRepositoryMock, never()).save(any());
+    }
+
+    @Test
+    public void erroSalvarProdutoCategoriaInexistenteTest() {
+        Long codigoCategoriaInexistente = 3L;
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produto = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1, PRECO_CUSTO_PRODUTO_1,
+                PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        assertThrows(RegraNegocioException.class, () -> produtoService.salvar(codigoCategoriaInexistente, produto));
+
+        verify(categoriaServiceMock, times(1)).buscarPorCodigo(any());
+        verify(produtoRepositoryMock, never()).save(any());
+    }
+
+    @Test
+    public void erroSalvarProdutoDuplicadoTest() {
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produtoExistente = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1,
+                PRECO_CUSTO_PRODUTO_1, PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+        Produto produtoNovo = createProduto(ID_PRODUTO_2, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1,
+                PRECO_CUSTO_PRODUTO_1, PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        doReturn(Optional.of(categoria)).when(categoriaServiceMock).buscarPorCodigo(ID_CATEGORIA_1);
+        doReturn(Optional.of(produtoExistente)).when(produtoRepositoryMock)
+                .findByCategoriaCodigoAndDescricao(ID_CATEGORIA_1, DESCRICAO_PRODUTO_1);
+
+        assertThrows(RegraNegocioException.class, () -> produtoService.salvar(ID_CATEGORIA_1, produtoNovo));
+
+        verify(categoriaServiceMock, times(1)).buscarPorCodigo(any());
+        verify(produtoRepositoryMock, times(1))
+                .findByCategoriaCodigoAndDescricao(anyLong(), anyString());
+        verify(produtoRepositoryMock, never()).save(any());
+    }
+
+    @Test
+    public void atualizarProdutoTest() {
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produto = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1, PRECO_CUSTO_PRODUTO_1,
+                PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        doReturn(Optional.of(produto)).when(produtoRepositoryMock).buscaPorCodigoProduto(ID_PRODUTO_1, ID_CATEGORIA_1);
+        doReturn(Optional.of(categoria)).when(categoriaServiceMock).buscarPorCodigo(ID_CATEGORIA_1);
+        doReturn(Optional.of(produto)).when(produtoRepositoryMock)
+                .findByCategoriaCodigoAndDescricao(ID_CATEGORIA_1, DESCRICAO_PRODUTO_1);
+        doReturn(produto).when(produtoRepositoryMock).save(any());
+
+        Produto produtoSalvo = produtoService.atualizar(ID_CATEGORIA_1, ID_PRODUTO_1, produto);
+
+        verify(produtoRepositoryMock, times(1)).buscaPorCodigoProduto(anyLong(), anyLong());
+        verify(categoriaServiceMock, times(1)).buscarPorCodigo(anyLong());
+        verify(produtoRepositoryMock, times(1))
+                .findByCategoriaCodigoAndDescricao(anyLong(), anyString());
+        verify(produtoRepositoryMock, times(1)).save(any());
+
+        assertEquals(produto.getCodigo(),     produtoSalvo.getCodigo());
+        assertEquals(produto.getDescricao(),  produtoSalvo.getDescricao());
+        assertEquals(produto.getQuantidade(), produtoSalvo.getQuantidade());
+        assertEquals(produto.getPrecoCusto(), produtoSalvo.getPrecoCusto());
+        assertEquals(produto.getPrecoVenda(), produtoSalvo.getPrecoVenda());
+        assertEquals(produto.getObservacao(), produtoSalvo.getObservacao());
+        assertEquals(produto.getCategoria().getCodigo(), produtoSalvo.getCategoria().getCodigo());
+        assertEquals(produto.getCategoria().getNome(),   produtoSalvo.getCategoria().getNome());
+    }
+
+    @Test
+    public void erroAtualizarProdutoInexistenteTest() {
+        Categoria categoria = createCategoria(ID_CATEGORIA_1, NOME_CATEGORIA_TECNOLOGIA);
+
+        Produto produto = createProduto(ID_PRODUTO_1, DESCRICAO_PRODUTO_1, QUANTIDADE_PRODUTO_1, PRECO_CUSTO_PRODUTO_1,
+                PRECO_VENDA_PRODUTO_1, OBSERVACAO_PRODUTO_1, categoria);
+
+        doReturn(Optional.empty())
+                .when(produtoRepositoryMock).buscaPorCodigoProduto(ID_PRODUTO_1, ID_CATEGORIA_1);
+
+        assertThrows(EmptyResultDataAccessException.class, () ->
+                produtoService.atualizar(ID_CATEGORIA_1, ID_PRODUTO_1, produto));
+
+        verify(produtoRepositoryMock, times(1)).buscaPorCodigoProduto(anyLong(), anyLong());
+        verify(produtoRepositoryMock, never()).save(any());
     }
 
     private Categoria createCategoria(Long codCategoria, String nome) {
