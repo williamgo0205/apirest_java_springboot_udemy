e4506f636644376af6c23ab668e84c43ce5e6457
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-04-10 10:27:03 -0300

    Session 8 classroom 50 - Adding mapping JPA entities Venda and ItemVenda

---
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/entity/ItemVenda.java
@@ -0,0 +1,91 @@
+package com.vendas.gestaovendas.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "item_venda")
+public class ItemVenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "preco_vendido")
+    private BigDecimal precoVendido;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_produto", referencedColumnName = "codigo")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_venda", referencedColumnName = "codigo")
+    private Venda venda;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoVendido() {
+        return precoVendido;
+    }
+
+    public void setPrecoVendido(BigDecimal precoVendido) {
+        this.precoVendido = precoVendido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, quantidade, precoVendido, produto, venda);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemVenda itemVenda = (ItemVenda) o;
+        return Objects.equals(codigo, itemVenda.codigo) && Objects.equals(quantidade, itemVenda.quantidade)
+                && Objects.equals(precoVendido, itemVenda.precoVendido) && Objects.equals(produto, itemVenda.produto)
+                && Objects.equals(venda, itemVenda.venda);
+    }
+}
---
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/entity/Venda.java
@@ -0,0 +1,67 @@
+package com.vendas.gestaovendas.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "venda")
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "data")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_cliente", referencedColumnName = "codigo")
+    private Cliente cliente;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, data, cliente);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Venda venda = (Venda) o;
+        return Objects.equals(codigo, venda.codigo) && Objects.equals(data, venda.data)
+                && Objects.equals(cliente, venda.cliente);
+    }
+}
