022ffd087c5cd10ca502c657e67edfb60fea0843
William Goncalves de Oliveira <williamgo0205@gmail.com>
2022-04-23 15:00:22 -0300

    Session 8 classroom 58 - Adding Controller Vendas with method Post Vendas

--- gestao-vendas/src/main/java/com/vendas/gestaovendas/controller/VendaController.java
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/controller/VendaController.java
@@ -1,13 +1,17 @@
 package com.vendas.gestaovendas.controller;
 
 import com.vendas.gestaovendas.dto.venda.model.ClienteVendaResponseDTO;
+import com.vendas.gestaovendas.dto.venda.model.VendaRequestDTO;
 import com.vendas.gestaovendas.service.VendaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +39,15 @@ public class VendaController {
     public ResponseEntity<ClienteVendaResponseDTO> listarVendaPorCodigo(@PathVariable(name = "codigoVenda")
                                                                                       Long codigoVenda) {
         return ResponseEntity.ok(vendaService.listarVendaPorCodigo(codigoVenda));
+    }
+
+    // POST - localhost:8080/venda/cliente/{codigoCliente}
+    @ApiOperation(value = "Registar Venda",
+            nickname = "registarVenda")
+    @PostMapping("/cliente/{codigoCliente}")
+    public ResponseEntity<ClienteVendaResponseDTO> salvar(@PathVariable(name = "codigoCliente") Long codigoCliente,
+                                                          @RequestBody VendaRequestDTO vendaRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(vendaService.salvar(codigoCliente,vendaRequestDTO));
     }
 }
